862f74349f49cc3bbe38eb62ad83628231f16b1d
Miguelqf2077 <161902082+Miguelqf2077@users.noreply.github.com>
2024-04-09 09:49:49 -0400

    preuba 48

--- src/exercises.cpp
+++ src/exercises.cpp
@@ -34,27 +34,7 @@ int start = 0;
 
 void exercise_3(string s1) {
   // TODO: YOUR CODE HERE
-     string s1;
-    char visitas = 0;
-	
-	while(true){
-	
-	if(s1>="1"){
-      cout << "Om-nom-nom :P" << endl;
-      } else if(s1>="0"){
-      cout << "No cake :(" << endl;
-      break;
-     }
-    
-    visitas++;
-    
-    if (visitas >= 10) {
-            cout << "Se ha superado el limite de visitas. ¡Ya no queda más pastel!" << endl;
-            break;
-        }
-    
-    }
-
+  
    
 }
 
