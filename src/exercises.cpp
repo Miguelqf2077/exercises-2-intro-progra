ad949eff7f4705cb5df868726d750976f6479d50
Miguelqf2077 <161902082+Miguelqf2077@users.noreply.github.com>
2024-04-11 11:00:13 -0400

    74

--- src/exercises.cpp
+++ src/exercises.cpp
@@ -97,6 +97,33 @@ void exercise_4(int n) {
 
 void exercise_5(int n, int k) {
   // TODO: YOUR CODE HERE
+  int current_day = 1;
+
+    
+    for (int i = 1; i < n; ++i) {
+        cout << "   ";
+    }
+
+  
+    for (int i = n; i <= 7; ++i) {
+        if (current_day < 10) cout << " ";
+        if (current_day > k) cout << "- ";
+        else cout << current_day << " ";
+        if (current_day == k) break;
+        current_day++;
+    }
+
+  
+    while (current_day < k) {
+        cout << endl;
+        for (int i = 1; i <= 7; ++i) {
+            if (current_day > k) cout << "- ";
+            else cout << (current_day < 10 ? " " : "") << current_day << " ";
+            if (current_day == k) break;
+            current_day++;
+        }
+    }
+
 }
 
 int exercise_6(int n) {
